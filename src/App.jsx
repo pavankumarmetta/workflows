5589872a8fbd0262b8eda1c4f5da7a9918bb5663
pavankumarmetta <pavankumarmetta01@gmail.com>
2025-07-29 18:46:17 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React Logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((prev) => prev + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello </h1>
     </>
   )
 }
